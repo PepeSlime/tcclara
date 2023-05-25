4f45a5e8f250f3bf58899f79d8a237bc3d275850
Jeniffer Scarpini <j.scarpiniquintam@gmail.com>
2023-05-25 20:07:35 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -28,26 +28,56 @@ Route::get('/professor/cadastro', function () {
     return view('cad_prof');
 });
 
+Route::get('/professor/cadastro', [ProfessorController::class, 'criar']);
+
+
+
 Route::get('/turma/cadastro', function () {
     return view('cad_turma');
 });
+
+Route::get('/turma/cadastro', [TurmaController::class, 'criar']);
+
+
+
 
 Route::get('/login', function () {
     return view('login');
 });
 
+Route::get('/login', [LoginController::class, 'logar']);
+
+
+
+
 Route::get('/componente/listagem', function () {
     return view('listagem_comp');
 });
+
+Route::get('/componente/listagem', [ComponenteController::class, 'Listar']);
+
+
+
+
 
 Route::get('/professor/listagem', function () {
     return view('listagem_prof');
 });
 
+Route::get('/componente/listagem', [ComponenteController::class, 'Listar']);
+
+
+
+
 Route::get('/turma/listagem', function () {
     return view('listagem_turma');
 });
+Route::get('/turma/listagem', [TurmaController::class, 'Listar']);
+
+
+
 
 Route::get('/horario', function () {
     return view('horario');
 });
+Route::get('/horario', [HorarioController::class, 'Listar']);
