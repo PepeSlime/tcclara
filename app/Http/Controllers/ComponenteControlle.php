20a647bf9d57db4e050277065422e05d90e58039
Jeniffer Scarpini <j.scarpiniquintam@gmail.com>
2023-05-24 22:27:40 -0300

    controler

---
+++ app/Http/Controllers/ComponenteControlle.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ComponenteControlle extends Controller
+{
+    public function criar()
+	{
+		return view('cad_comp');
+	}
+
+	public function listar()
+	{
+		return view('listagem_comp');
+	}
+
+	public function alterar()
+	{
+		return view('cad_comp'); 
+	}
+
+	public function excluir()
+	{
+		// Exclui um registro
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('inicio');
-});
 
-Route::get('/componente/cadastro', function () {
-    return view('cad_comp');
-});
+    
+
+
+Route::get('/', [LoginController::class, 'logar']);
+
+
+
+Route::get('/componente/cadastro', [ComponentController::class, 'criar']);
 
 
 Route::get('/professor/cadastro', function () {
