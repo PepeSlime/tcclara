a80019eb5bb46180e4b023191e73903182889057
Pedro Henrique <pepegemeo8@gmail.com>
2023-05-26 20:44:52 -0300

    login

--- resources/views/cad_comp.blade.php
+++ resources/views/cad_comp.blade.php
@@ -1,4 +1,5 @@
-<x-layout>
+<!DOCTYPE html>
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
@@ -12,7 +13,58 @@
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 
-<section>
+<body>
+
+
+    <header id="header" class="flex-row content-vcenter space-40 content-space-between box">
+        <span class="material-symbols-outlined">
+            menu
+        </span>
+
+        <h1>
+            Timenize
+        </h1>
+
+        <span class="material-symbols-outlined">
+            account_circle
+        </span>
+    </header>
+
+    <div class="conteudo grow">
+
+        <nav id="menu" class="flex-col box fit-width space-30">
+            <a href="" class="flex-row gap-10 content-vcenter">
+                <span class="material-symbols-outlined">
+                    home
+                </span>
+                <span>
+                    Home
+                </span>
+            </a>
+
+            <a href="" class="flex-row gap-10 content-vcenter">
+                <span class="material-symbols-outlined">
+                    settings
+                </span>
+
+                </span>
+                <span>
+                    Configurações
+                </span>
+            </a>
+
+            <a href="" class="flex-row gap-10 content-vcenter">
+                <span class="material-symbols-outlined">
+                    logout
+                </span>
+                <span>
+                    Logout
+                </span>
+            </a>
+
+
+        </nav>
+        <section>
             <form id="form">
                 <div>
                     <label>Cadastro Componente</label>
@@ -29,5 +81,10 @@
 
     </div>
 
+    <footer class="flex-row content-center box space-40 ">
+        <div>JJP 2023</div>
+    </footer>
 
-</x-layout>
+</body>
+
+</html>
--- resources/views/cad_prof.blade.php
+++ resources/views/cad_prof.blade.php
@@ -6,28 +6,27 @@
     <link rel="stylesheet" href="/flex/flex-style.css">
     <link rel="stylesheet" href="/flex/flex.css">
     <link rel="stylesheet" href="/css/prof.css">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-        <script src="/js/home.js"></script>
-        <link rel="stylesheet" href="home.js">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <script src="/js/home.js"></script>
+    <link rel="stylesheet" href="/css/home.js">
 </head>
 
 <section>
-            <form>
-                <div>
-                    <label>Cadastro Professor</label>
-                </div>
-                <div>
-                    <input type="text" placeholder="Nome:" name="name" required>
-                </div>
-                <div>
-                    <input type="text" placeholder="e-mail:" name="email" required>
-                </div>
-                <div>
-                    <input type="text" placeholder="Telefone:" name="telefone" required>
-                </div>
-                <div>
-                    <input type="submit" value="Cadastrar">
-                </div>
-            </form>
-        </section>
+    <form>
+        <div>
+            <label>Cadastro Professor</label>
+        </div>
+        <div>
+            <input type="text" placeholder="Nome:" name="name" required>
+        </div>
+        <div>
+            <input type="text" placeholder="e-mail:" name="email" required>
+        </div>
+        <div>
+            <input type="text" placeholder="Telefone:" name="telefone" required>
+        </div>
+        <div>
+            <input type="submit" value="Cadastrar">
+        </div>
+    </form>
+</section>
