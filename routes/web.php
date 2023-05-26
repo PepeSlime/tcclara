4d2cc116ee670c23ed09ce2b59df11b37aff27e3
Jeniffer Scarpini <j.scarpiniquintam@gmail.com>
2023-05-26 20:45:17 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,83 +1,31 @@
 <?php
 
+use App\Http\Controllers\ComponenteController;
+use App\Http\Controllers\HorarioController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfessorController;
+use App\Http\Controllers\TurmaController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-
-    
 
 
 Route::get('/', [LoginController::class, 'logar']);
 
 
+Route::get('/componente/cadastro', [ComponenteController::class, 'criar']);
+Route::get('/componente/edicao', [ComponenteController::class, 'criar']);
+Route::get('/componente/listagem', [ComponenteController::class, 'listar']);
 
-Route::get('/componente/cadastro', [ComponentController::class, 'criar']);
-
-
-Route::get('/professor/cadastro', function () {
-    return view('cad_prof');
-});
 
 Route::get('/professor/cadastro', [ProfessorController::class, 'criar']);
+Route::get('/professor/edicao', [ProfessorController::class, 'criar']);
+Route::get('/professor/listagem', [ProfessorController::class, 'listar']);
 
-
-
-Route::get('/turma/cadastro', function () {
-    return view('cad_turma');
-});
 
 Route::get('/turma/cadastro', [TurmaController::class, 'criar']);
+Route::get('/turma/edicao', [TurmaController::class, 'criar']);
+Route::get('/turma/listagem', [TurmaController::class, 'listar']);
 
 
 
 
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/login', [LoginController::class, 'logar']);
-
-
-
-
-Route::get('/componente/listagem', function () {
-    return view('listagem_comp');
-});
-
-Route::get('/componente/listagem', [ComponenteController::class, 'Listar']);
-
-
-
-
-
-Route::get('/professor/listagem', function () {
-    return view('listagem_prof');
-});
-
-Route::get('/componente/listagem', [ComponenteController::class, 'Listar']);
-
-
-
-
-Route::get('/turma/listagem', function () {
-    return view('listagem_turma');
-});
-Route::get('/turma/listagem', [TurmaController::class, 'Listar']);
-
-
-
-
-Route::get('/horario', function () {
-    return view('horario');
-});
-Route::get('/horario', [HorarioController::class, 'Listar']);
+Route::get('/horario', [HorarioController::class, 'listar']);
