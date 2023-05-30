ba68ddf174bcfe04d938da2a9068b84e8e380bd7
José Henrique <zezegemeo8@gmail.com>
2023-05-30 20:13:11 -0300

    listagemprofessor

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,7 +1,15 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Timenize</title>
+    <link rel="stylesheet" href="/flex/flex-style.css">
+    <link rel="stylesheet" href="/flex/flex.css">
+    <link rel="stylesheet" href="g-icons.css" />
+</head>
 
 <body>
     <header id="header" class="flex-row content-vcenter space-40  content-space-between box">
--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,35 +1,25 @@
 <x-layout>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" href="/flex/flex-style.css">
-    <link rel="stylesheet" href="/flex/flex.css">
-    <link rel="stylesheet" href="/css/home.css">
 
-    <link rel="stylesheet" href="g-icons.css" />
+    <title>Home</title>
+    <link rel="stylesheet" href="/css/home.css">
     <script src="/js/home.js"></script>
 
-
-</head>
-
-</nav>
-        <div class="flex-row content-center space-30">
-            <div  class="box-round">
-                <img onclick="vaiProf()" src="/img/professor.png">
-                <h2>Professor</h2>
-            </div>
-
-            <div class="box-round">
-                <img onclick="vaiComp()" src="/img/livros.png">
-                <h2>Componente</h2>
-            </div>
-
-            <div class="box-round">
-                <img src="/img/relogio.png">
-                <h2>Horário</h2>
-            </div>
+    </nav>
+    <div class="flex-row content-center space-30">
+        <div class="box-round">
+            <img onclick="vaiProf()" src="/img/professor.png">
+            <h2>Professor</h2>
         </div>
+
+        <div class="box-round">
+            <img onclick="vaiComp()" src="/img/livros.png">
+            <h2>Componente</h2>
+        </div>
+
+        <div class="box-round">
+            <img src="/img/relogio.png">
+            <h2>Horário</h2>
+        </div>
+    </div>
     </div>
 </x-layout>
