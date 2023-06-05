bde2d5c06f9cc150186307655edca141d096c7ea
Jeniffer Scarpini <j.scarpiniquintam@gmail.com>
2023-06-05 20:01:22 -0300

    listagem

--- resources/views/listagem_turma.blade.php
+++ resources/views/listagem_turma.blade.php
@@ -17,7 +17,7 @@
                 <td class="actions">
 
                     <a class="btm" href="/turma/criar/{{ $turma->nome }}" class="btn-editar">Editar</a>
-                    <a class="btm" href="/turma/horario/{{ $turma->id }}" class="btn-excluir">Excluir</a>
+                    <a class="btm" href="/turma/horario/{{ $turma->id }}" class="btn-Wexcluir">Excluir</a>
                     <a class="btm" href="/turma/excluir/{{ $turma->id }}" class="btn-excluir">Excluir</a>
 
 
@@ -26,7 +26,7 @@
             </tr>
             @endforeach
             
-
+W
 
 
         </tbody>
